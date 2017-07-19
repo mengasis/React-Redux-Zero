5f2357171daab2ce91828374d852f89e6a0a5d2c
Manuel Venegas <mengasis@outlook.com>
2017-07-19 11:40:35 -0400

    Configuracion en el index de las librerias

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import {render} from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Bluebird from 'bluebird'
+import { Provider } from 'react-redux'
 
 import './index.css'
 import AppRoutes from './routes'
 import registerServiceWorker from './registerServiceWorker'
 
+import configureStore from './lib/configureStore'
+
+import rootReducer from './reducers'
+
+// Bluebird pisara a las promesas por defecto
+//Configuracion de Bluebird
+window.Promise = Bluebird
+Bluebird.config({ warning: false })
+
+window.addEventListener('unhandledrejection', error => {
+	error.preventDefault()
+
+	if(process.env.NODE_ENV !== 'production') {
+		console.warn('Warning Promises', error.detail)
+	} 
+})
+
+
+//Configurar Store
+const store = configureStore({
+	initialState: window.initialState
+}, rootReducer)
+
 render(
-	<Router>
-		<AppRoutes />
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<AppRoutes />
+		</Router>
+	</Provider>
 	, document.getElementById('root'))
 registerServiceWorker()
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import { Route, Switch } from 'react-router-dom'
 import App from './components/App'
 import About from './components/About'
 import Contact from './components/Contact'
-import Home from './components/Home'
+import Home from './conteiners/Home'
 import Page404 from './components/Page404'
 
 const AppRoutes = () =>
@@ -14,7 +14,7 @@ const AppRoutes = () =>
 		<Switch>
 			<Route path="/about" component={About} />
 			<Route path="/contact" component={Contact} />
-			<Route path="/home" component={Home} />
+			<Route exact path="/" component={Home} />
 			<Route component={Page404} />
 		</Switch>
 	</App>
